237cfc8f144ee3fb091b24e674ec437f3f9a4f76
zenice@gmail.com <dltkdal3!>
2019-08-02 10:50:02 +0900

    게시판 페이징 예제 완료

--- src/main/java/com/bit/academy/model/BoardPaging.java
+++ src/main/java/com/bit/academy/model/BoardPaging.java
@@ -1,14 +1,17 @@
 package com.bit.academy.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardPaging {
 
     private int totalCount; // 전체 게시물 갯수
 
     private int currentPage; // 현재 페이지 수
 
-    private int articleCount; // 한 페이지당 게시물 갯수 - 20
+    private int articleCount = 20; // 한 페이지당 게시물 갯수 - 20
 
-    private int pageCount; // 1 2 3 4 ~ 10 // 페이지 갯수
+    private int pageCount = 10; // 1 2 3 4 ~ 10 // 페이지 갯수
 
     private int prevPage; // 이전 페이지
 
@@ -20,8 +23,88 @@ public class BoardPaging {
 
     private String searchKeyword; // 검색어
 
-
     private int limitBegin;
+
+
+    public static void main(String[] args){
+
+        BoardPaging p = new BoardPaging();
+
+        p.setTotalCount(2500);
+        p.setCurrentPage(122);
+
+        System.out.print(p.getPrevPage() + " <    ");
+
+        List<Integer> pageRange = p.getPageRange();
+
+        for(int page:pageRange){
+            if(p.getCurrentPage()==page) System.out.print("[");
+
+            System.out.print(page);
+
+            if(p.getCurrentPage()==page) System.out.print("]");
+
+            System.out.print(" ");
+
+        }
+
+        System.out.print("    > "+p.getNextPage());
+
+    }
+
+    private List<Integer> pageRange;
+
+    public List<Integer> getPageRange(){
+        pageRange = new ArrayList<>();
+
+        int begin = currentPage / pageCount * pageCount + 1;
+
+        if(currentPage%pageCount==0){
+            begin-=pageCount;
+        }
+
+        int end = begin + pageCount -1 ;
+
+        if(end>getLastPage()){
+            end = getLastPage();
+        }
+
+        for( int i=begin ; i <= end ; i++){
+            pageRange.add(i);
+        }
+
+        return pageRange;
+    }
+
+    public int getPrevPage(){
+        this.prevPage = this.getPageRange().get(0);
+
+        // 현재페이지가 pageCount 보다 작거나 같은 경우 0. 이전 첫 페이지 없음
+        if(this.currentPage <= pageCount){
+            this.prevPage = 0;
+        }
+        else{
+            this.prevPage-=1;
+        }
+        return this.prevPage;
+    }
+
+    public int getNextPage(){
+
+        this.nextPage = this.currentPage;
+
+        if(this.nextPage%pageCount>0){
+            this.nextPage+=pageCount;
+        }
+
+        this.nextPage = this.nextPage / pageCount * pageCount +1;
+
+        if(this.nextPage>this.getLastPage()){
+            this.nextPage = 0;
+        }
+
+        return this.nextPage;
+    }
 
     /**
      * MyBatis LIMIT 시작
@@ -63,16 +146,8 @@ public class BoardPaging {
         this.pageCount = pageCount;
     }
 
-    public int getPrevPage() {
-        return prevPage;
-    }
-
     public void setPrevPage(int prevPage) {
         this.prevPage = prevPage;
-    }
-
-    public int getNextPage() {
-        return nextPage;
     }
 
     public void setNextPage(int nextPage) {
@@ -89,8 +164,11 @@ public class BoardPaging {
     }
 
     public int getLastPage() {
+        lastPage = this.totalCount / this.articleCount;
 
-        lastPage = this.totalCount / this.articleCount + 1;
+        if(this.totalCount % this.articleCount>0){
+            lastPage++;
+        }
 
         return lastPage;
     }
